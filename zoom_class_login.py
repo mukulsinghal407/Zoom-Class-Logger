3722d0354fe1beb395f9218f83cee60998466cd3
mukulsinghal407 <mukulsinghal407@gmail.com>
2022-01-22 13:18:39 +0530

    Weekends handled

--- zoom_class_login.py
+++ zoom_class_login.py
@@ -2,19 +2,19 @@ import xlrd as excel
 import webbrowser as wbs
 from datetime import date 
 
-numberofclasses = [5,7,5,4,4]
+numberofclasses = [5,7,5,4,4,0,0]
+weekend = ["Saturday","Sunday"]
 
-def login(number):
+def login(number,today):
     check=0
-    wb = excel.open_workbook(r"C:\Users\ASUS\OneDrive\Pictures\Screenshots\OneDrive\Desktop\Timetable.xlsx")
+    wb = excel.open_workbook(r"C:\Users\ASUS\OneDrive\Pictures\Screenshots\OneDrive\Desktop\Zoom-Class-Logger\Timetable.xlsx")
     sheet = wb.sheet_by_index(0)
-    today = date.today().strftime("%A")
     i=0
     for i in range(sheet.ncols):
         if today in sheet.cell_value(0,i):
             link = sheet.cell_value(number,i)
             print("Loging into zoom link ",link)
-            #wbs.open(link)
+            wbs.open(link)
             break
     if numberofclasses[i] == number:
         check=1
@@ -24,9 +24,13 @@ def login(number):
 def main():
     i=1
     check=0
+    today = date.today().strftime("%A");
+    for i in weekend:
+        if i==today:
+            return
     print("Press Enter to proceed")
     while((not check) and (input()=="" or input())):
-        lis=login(i)
+        lis=login(i,today)
         i=lis[0]
         check=lis[1]
 main()
